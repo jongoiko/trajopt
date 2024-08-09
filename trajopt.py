3c3b5ad86065cbf76b13c7807b58794567f82a4e
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2024-08-09 12:04:25 +0200

    Recalculate problem attributes on every _build_nlp call

--- trajopt.py
+++ trajopt.py
@@ -2,6 +2,8 @@ import cyipopt
 import numpy as np
 import jax
 import jax.numpy as jnp
+import scipy.integrate
+import types
 from collocation.util import _pack, _unpack, _get_time
 from collocation import trapezoidal, hermite_simpson
 
@@ -76,68 +78,80 @@ class OCP:
         self._x_f_lower, self._x_f_upper = x_f
         self._x_lower, self._x_upper = x_bounds
         self._u_lower, self._u_upper = u_bounds
-        self._x_shape = (n_grid, self._x_0_lower.size)
-        self._u_shape = (
-            n_grid if not self._u_midpoints else 2 * n_grid - 1,
-            initial_guess._u.shape[1],
-        )
         self._time_fractions = jnp.linspace(0, 1, n_grid)
-        self.objective = lambda x_u: self._method._objective(
-            x_u,
-            self._time_fractions,
-            self._running_cost,
-            self._dynamics,
-            self._x_shape,
-            self._u_shape,
+        self._initial_guess = initial_guess
+        self._solver_kwargs = solver_kwargs
+        self._jac_sparsity_estimation_samples = jac_sparsity_estimation_samples
+
+    def _estimate_jacobian_structure(
+        self, n_samples, initial_x_u, constraints, seed=42
+    ):
+        key = jax.random.key(seed)
+        key, *subkeys = jax.random.split(key, n_samples)
+        get_jacobian = jax.jit(jax.jacobian(constraints))
+        jac_sum = np.zeros(get_jacobian(initial_x_u).shape)
+        for subkey in subkeys:
+            perturbation = jax.random.normal(subkey, shape=initial_x_u.shape)
+            jacobian = get_jacobian(initial_x_u + perturbation)
+            jac_sum += jnp.abs(jacobian)
+        return jnp.nonzero(jac_sum)
+
+    def _pack_initial_guess(self):
+        guess_t_0, guess_t_f = (
+            self._initial_guess._t.min(),
+            self._initial_guess._t.max(),
         )
-        self.gradient = jax.jit(jax.grad(self.objective))
-        self.constraints = lambda x_u: self._method._collocation_constraints(
-            x_u, self._time_fractions, self._dynamics, self._x_shape, self._u_shape
-        )
-        self._n_grid = n_grid
-        guess_t_0, guess_t_f = initial_guess._t.min(), initial_guess._t.max()
-        self._initial_guess = _pack(
-            *initial_guess.interpolate(
-                jnp.linspace(guess_t_0, guess_t_f, n_grid),
+        return _pack(
+            *self._initial_guess.interpolate(
+                _get_time(guess_t_0, guess_t_f, self._time_fractions),
                 self._u_midpoints,
             ),
             guess_t_0,
             guess_t_f,
         )
-        self._jacobian_structure = self._estimate_jacobian_structure(
-            jac_sparsity_estimation_samples
-        )
-        self.jacobianstructure = jax.jit(lambda: self._jacobian_structure)
-        self.jacobian = jax.jit(
-            lambda x_u: jax.jacobian(self.constraints)(x_u)[self._jacobian_structure]
-        )
-        self._nlp = self._build_nlp()
-        if solver_kwargs is None:
-            return
-        for key, value in solver_kwargs.items():
-            self._nlp.add_option(key, value)
 
-    def _estimate_jacobian_structure(self, n_samples, seed=42):
-        key = jax.random.key(seed)
-        key, *subkeys = jax.random.split(key, n_samples)
-        get_jacobian = jax.jit(jax.jacobian(self.constraints))
-        jac_sum = np.zeros(get_jacobian(self._initial_guess).shape)
-        for subkey in subkeys:
-            perturbation = jax.random.normal(subkey, shape=self._initial_guess.shape)
-            jacobian = get_jacobian(self._initial_guess + perturbation)
-            jac_sum += jnp.abs(jacobian)
-        return jnp.nonzero(jac_sum)
+    def _get_x_u_shapes(self):
+        n_grid = self._time_fractions.size
+        x_shape = (n_grid, self._x_0_lower.size)
+        u_shape = (
+            n_grid if not self._u_midpoints else 2 * n_grid - 1,
+            self._u_lower.size,
+        )
+        return x_shape, u_shape
 
-    def _build_nlp(self):
-        n = self._initial_guess.size
-        m = self.constraints(self._initial_guess).size
+    def _build_nlp(self, packed_initial_guess):
+        n_grid = self._time_fractions.size
+        x_shape, u_shape = self._get_x_u_shapes()
+        problem = types.SimpleNamespace()
+        problem.objective = lambda x_u: self._method._objective(
+            x_u,
+            self._time_fractions,
+            self._running_cost,
+            self._dynamics,
+            x_shape,
+            u_shape,
+        )
+        problem.gradient = jax.jit(jax.grad(problem.objective))
+        constraints = lambda x_u: self._method._collocation_constraints(
+            x_u, self._time_fractions, self._dynamics, x_shape, u_shape
+        )
+        problem.constraints = constraints
+        jacobian_structure = self._estimate_jacobian_structure(
+            self._jac_sparsity_estimation_samples, packed_initial_guess, constraints
+        )
+        problem.jacobianstructure = jax.jit(lambda: jacobian_structure)
+        problem.jacobian = jax.jit(
+            lambda x_u: jax.jacobian(constraints)(x_u)[jacobian_structure]
+        )
+        n = packed_initial_guess.size
+        m = problem.constraints(packed_initial_guess).size
         lb_x, ub_x = [
-            jnp.tile(arr, (self._n_grid, 1)) for arr in [self._x_lower, self._x_upper]
+            jnp.tile(arr, (n_grid, 1)) for arr in [self._x_lower, self._x_upper]
         ]
         lb_u, ub_u = [
             jnp.tile(
                 arr,
-                (self._n_grid if not self._u_midpoints else 2 * self._n_grid - 1, 1),
+                (n_grid if not self._u_midpoints else 2 * n_grid - 1, 1),
             )
             for arr in [self._u_lower, self._u_upper]
         ]
@@ -147,13 +161,21 @@ class OCP:
             ub_x, ub_u, self._t_0_upper, self._t_f_upper
         )
         zeros = jnp.zeros(m)
-        return cyipopt.Problem(
-            problem_obj=self, n=n, m=m, cl=zeros, cu=zeros, lb=lb, ub=ub
+        problem = cyipopt.Problem(
+            problem_obj=problem, n=n, m=m, cl=zeros, cu=zeros, lb=lb, ub=ub
         )
+        if self._solver_kwargs is None:
+            return problem
+        for key, value in self._solver_kwargs.items():
+            problem.add_option(key, value)
+        return problem
 
     def solve(self):
-        x_u, _ = self._nlp.solve(self._initial_guess)
-        x, u, t_0, t_f = _unpack(x_u, self._x_shape, self._u_shape)
-        self._nlp.close()
+        initial_x_u = self._pack_initial_guess()
+        nlp = self._build_nlp(initial_x_u)
+        x_u, _ = nlp.solve(initial_x_u)
+        nlp.close()
+        x_shape, u_shape = self._get_x_u_shapes()
+        x, u, t_0, t_f = _unpack(x_u, x_shape, u_shape)
         t = _get_time(t_0, t_f, self._time_fractions)
         return self._trajectory_subclass(t, x, u, self._dynamics)
